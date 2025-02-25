9255bbbbec5a49499897f4a11b258fcd2feed2d4
rvif <ravijeeeetsharma4444@gmail.com>
2025-02-25 18:59:28 +0000

    add delete command

--- cmd/root.go
+++ cmd/root.go
@@ -47,7 +47,8 @@ func loadTasks() {
 
 // Save to JSON file
 func saveTasks() {
-	data, _ := json.MarshalIndent(tasks, "", "  ")
+	data, _ := json.MarshalIndent(tasks, "", "   ")
+	// log.Println(string(data))
 	os.WriteFile(taskFile, data, 0644)
 }
 
