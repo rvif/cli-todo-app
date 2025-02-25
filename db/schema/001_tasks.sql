af002bb078ba5fab50586192c8e250de9e1559f9
rvif <ravijeeeetsharma4444@gmail.com>
2025-02-25 19:50:55 +0000

    add tasks schema for goose

--- db/schema/001_tasks.sql
+++ db/schema/001_tasks.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE tasks (
     id TEXT PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     is_completed BOOLEAN NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
