a70e4a0e5be79e57431adaff93e6d90941768272
rvif <ravijeeeetsharma4444@gmail.com>
2025-02-27 18:45:54 +0000

    remove json as data storage solution

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"database/sql"
-	"encoding/json"
 	"os"
 	"time"
 
@@ -29,40 +28,12 @@ type Task struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-// Temp json solution for storing tasks
-var tasks []Task
-
-const taskFile = "tasks.json"
-
 // global postgress connection
 var db *sql.DB
-
-// Load tasks from JSON file (or create if missing)
-func loadTasks() {
-	if _, err := os.Stat(taskFile); os.IsNotExist(err) {
-		os.WriteFile(taskFile, []byte("[]"), 0644) // 0644: read and write the file or directory and other users can only read it.
-		return
-	}
-
-	file, err := os.ReadFile(taskFile)
-	if err == nil {
-		json.Unmarshal(file, &tasks)
-	}
-}
-
-// Save to JSON file
-func saveTasks() {
-	data, _ := json.MarshalIndent(tasks, "", "   ")
-	// log.Println(string(data))
-	os.WriteFile(taskFile, data, 0644)
-}
 
 var rootCmd = &cobra.Command{
 	Use:   "cli-todo-app",
 	Short: "A simple CLI todo app",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		loadTasks() // Load tasks before running any command
-	},
 }
 
 func Execute(conn *sql.DB) {
